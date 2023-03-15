9091db1abb58792e812aa5584274e6cbcc46fa15
ZeusOnline1998 <zeusonline1998@gmail.com>
2023-03-16 00:37:03 +0530

    Project Lombok - @Slf4j - Logging

--- src/main/java/in/zeusonline/spring6restmvc/controller/BeerController.java
+++ src/main/java/in/zeusonline/spring6restmvc/controller/BeerController.java
@@ -1,14 +1,24 @@
 package in.zeusonline.spring6restmvc.controller;
 
+import in.zeusonline.spring6restmvc.model.Beer;
 import in.zeusonline.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 
+import java.util.UUID;
+
+@Slf4j
 @AllArgsConstructor
 @Controller
 public class BeerController {
 
     private final BeerService beerService;
 
+    public Beer getBeerById(UUID id){
 
+        log.debug("Get Beer by id - in controller");
+
+        return beerService.getBeerId(id);
+    }
 }
