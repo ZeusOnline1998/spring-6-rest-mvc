209cf099559bc85ba35d005caacc3467e1ff717b
ZeusOnline1998 <zeusonline1998@gmail.com>
2023-03-16 00:48:57 +0530

    Project Lombok - DeLombok

---
+++ src/main/java/in/zeusonline/spring6restmvc/model/DeLombok.java
@@ -0,0 +1,72 @@
+package in.zeusonline.spring6restmvc.model;
+
+public class DeLombok {
+
+    private Integer id;
+    private String name;
+    private String address;
+
+    public DeLombok() {
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof DeLombok)) return false;
+        final DeLombok other = (DeLombok) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$id = this.getId();
+        final Object other$id = other.getId();
+        if (this$id == null ? other$id != null : !this$id.equals(other$id)) return false;
+        final Object this$name = this.getName();
+        final Object other$name = other.getName();
+        if (this$name == null ? other$name != null : !this$name.equals(other$name)) return false;
+        final Object this$address = this.getAddress();
+        final Object other$address = other.getAddress();
+        if (this$address == null ? other$address != null : !this$address.equals(other$address)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof DeLombok;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $id = this.getId();
+        result = result * PRIME + ($id == null ? 43 : $id.hashCode());
+        final Object $name = this.getName();
+        result = result * PRIME + ($name == null ? 43 : $name.hashCode());
+        final Object $address = this.getAddress();
+        result = result * PRIME + ($address == null ? 43 : $address.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "DeLombok(id=" + this.getId() + ", name=" + this.getName() + ", address=" + this.getAddress() + ")";
+    }
+}
