b606135a0da99d42e1a302f256ed835528a69fb1
ZeusOnline1998 <zeusonline1998@gmail.com>
2023-03-16 21:39:55 +0530

    Spring MVC - HTTP PATCH method

--- src/main/java/in/zeusonline/spring6restmvc/controller/BeerController.java
+++ src/main/java/in/zeusonline/spring6restmvc/controller/BeerController.java
@@ -20,6 +20,13 @@ public class BeerController {
 
     private final BeerService beerService;
 
+    @PatchMapping("{beerId}")
+    public ResponseEntity updateBeerPatchById(@PathVariable("beerId") UUID beerId, @RequestBody Beer beer){
+
+        beerService.patchBeerById(beerId, beer);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
     @DeleteMapping("/1/{beerId}")
     public ResponseEntity deleteBeerById(@PathVariable("beerId") UUID beerId) {
 
--- src/main/java/in/zeusonline/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/in/zeusonline/spring6restmvc/services/BeerServiceImpl.java
@@ -4,6 +4,7 @@ import in.zeusonline.spring6restmvc.model.Beer;
 import in.zeusonline.spring6restmvc.model.BeerStyle;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -110,6 +111,20 @@ public class BeerServiceImpl implements BeerService {
     public void deleteBeer(UUID beerId) {
 
         beerMap.remove(beerId);
+    }
+
+    @Override
+    public void patchBeerById(UUID beerId, Beer beer) {
+        Beer existing = beerMap.get(beerId);
+
+        if(StringUtils.hasText(beer.getBeerName())){
+            existing.setBeerName(beer.getBeerName());
+        }
+        if(beer.getBeerStyle()!= null){
+            existing.setBeerStyle(beer.getBeerStyle());
+        }
+
+        beerMap.put(existing.getId(), existing);
     }
 
 }
