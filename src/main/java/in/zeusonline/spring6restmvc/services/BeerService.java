69e24377231b1e86fccd0e179ec8ab3bacf6baeb
ZeusOnline1998 <zeusonline1998@gmail.com>
2023-03-16 13:47:06 +0530

    Spring MVC - List Beers

--- src/main/java/in/zeusonline/spring6restmvc/services/BeerService.java
+++ src/main/java/in/zeusonline/spring6restmvc/services/BeerService.java
@@ -2,9 +2,12 @@ package in.zeusonline.spring6restmvc.services;
 
 import in.zeusonline.spring6restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+
+    List<Beer> listBeers();
 
     Beer getBeerId(UUID id);
 }
